71279d6505bb4ff4b3cafe8e9489098e614dbb21
Meclotfi <hl_mecharbat@esi.dz>
2022-07-03 17:12:09 +0200

    add method

---
+++ BO.py
@@ -0,0 +1,123 @@
+from numpy import arange,vstack,argmax,asarray
+from numpy.random import random
+from scipy.stats import norm
+from sklearn.gaussian_process import GaussianProcessRegressor
+from warnings import catch_warnings,simplefilter
+import matplotlib.pyplot as plt
+import numpy as np
+from train import get_val_acc
+from .dataset import get_data_loaders
+ 
+class Problem():
+    def __init__(self):
+
+        # load the fashion mnist dataset into the pytorch data loaders
+        self.dataloaders,self.dataset_sizes=get_data_loaders()
+
+    def evaluate(self,lr):
+        acc=get_val_acc(lr,self.dataloaders,self.dataset_sizes)
+        return acc
+
+class BO():
+    def __init__(self,n_iters=10):
+        #
+        self.problem=Problem()
+        #initialize the surrogate
+        self.surrogate = GaussianProcessRegressor()
+        self.n_iters=n_iters
+
+    # probability of improvement acquisition function
+    def EI(X, Xsamples, model,xi=0.01):
+        mu, sigma = model.predict(X, return_std=True)
+        mu_sample = model.predict(Xsamples)
+
+        sigma = sigma.reshape(-1, 1)
+        
+        # Needed for noise-based model,
+        # otherwise use np.max(Y_sample).
+        # See also section 2.4 in [1]
+        mu_sample_opt = np.max(mu_sample)
+
+        with np.errstate(divide='warn'):
+            imp = mu - mu_sample_opt - xi
+            Z = imp / sigma
+            ei = imp * norm.cdf(Z) + sigma * norm.pdf(Z)
+            ei[sigma == 0.0] = 0.0
+        return ei
+    
+    def opt_acq(self,X, y, model,axis):
+        # random search, generate random samples
+        Xsamples = random(1000)
+        Xsamples = Xsamples.reshape(len(Xsamples), 1)
+        # calculate the acquisition function for each sample
+        eis = self.EI(Xsamples, X, model)
+        # locate the index of the largest scores
+        self.plt_acq(axis,Xsamples,eis)
+        ix = argmax(eis)
+        return Xsamples[ix, 0]
+    
+    def plot_surrogate(self,axis):
+        axis.set_xlabel('Learning rate')
+        axis.set_ylabel('Accuracy')
+        axis.set_title('Surrogate Gaussian Model')
+        
+        # scatter plot of inputs and real objective function
+        axis.scatter(self.X,self.Y)
+        
+        # line plot of the posterior mean
+        Xsamples = asarray(arange(0, 1, 0.001))
+        Xsamples = Xsamples.reshape(len(Xsamples), 1)
+        ysamples, y_std = self.surrogate.predict(Xsamples,return_std=True)                                                                                                                                                                                                                                                           
+        axis.plot(Xsamples, ysamples)
+
+        
+
+        #Plot the standard deviation
+        Xsamples = Xsamples.reshape(-1)
+        ysamples = ysamples.reshape(-1)
+        upper_bound=(ysamples - y_std).reshape(-1)
+        lower_bound=(ysamples + y_std).reshape(-1)
+        axis.fill_between(Xsamples,upper_bound,lower_bound,color='gray', alpha=0.2)
+
+        #Plot the last selected point 
+        axis.scatter(X[-1:],y[-1:],color='red',marker="x")
+    
+    def plot_acq(self,axis,Xsamples,ei):
+        # plot the acquisition 
+        arg=np.argsort(Xsamples.reshape(-1))
+        axis.set_xlabel('Learning rate')
+        axis.set_ylabel('Expected improvement')
+        axis.set_title('Acquisition function')
+        axis.plot(Xsamples[arg].reshape(-1),ei[arg].reshape(-1))
+
+    def Run(self):
+       X = random(1).reshape(len(X), 1)
+       Y = asarray([self.problem.evaluate(X[0])]).reshape(len(Y), 1)
+       self.X = X
+       self.Y = Y
+       fig, axs = plt.subplots(2)
+       fig.set_size_inches(12,10)
+       plt.subplots_adjust(left=0.1,bottom=0.1,right=0.9,top=0.9,wspace=0.4,hspace=0.4)
+        
+
+       for i in range(self.n_iter):
+            fig.suptitle('Plots of iteration '+str(i))
+            # fitting the suurogate
+            self.surrogate.fit(self.X, self.Y)
+
+            # optimizing the acquisition function and select the next point
+            x_next = self.opt_acq(self.X,self.Y,self.surrogate,axs[1])
+            
+            # evaluate the next point 
+            y_next = self.problem.evaluate(x_next)
+                
+            # add the point to the dataset
+            self.X = vstack((self.X, [[x_next]]))
+            self.Y = vstack((self.Y, [[y_next]]))
+
+            #Plot the all observations, the posterior mean, uncertainty estimate and the acquisition function after each iteration
+            self.plot_surrogate(axs[0])
+
+                
+            # return new data iteration by iteration
+            yield x_next, y_next
---
+++ train.py
@@ -0,0 +1,75 @@
+import time
+import copy
+import torch
+import torch.optim as optim
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+def train_model(model, criterion, optimizer, dataloaders, dataset_sizes, num_epochs=25):
+    since = time.time()
+
+    best_model_wts = copy.deepcopy(model.state_dict())
+    best_acc = 0.0
+
+    for epoch in range(num_epochs):
+        #print('Epoch {}/{}'.format(epoch, num_epochs - 1))
+        #print('-' * 10)
+
+        # Each epoch has a training and validation phase
+        for phase in ['train', 'val']:
+            if phase == 'train':
+                model.train()  # Set model to training mode
+            else:
+                model.eval()   # Set model to evaluate mode
+
+            running_loss = 0.0
+            running_corrects = 0
+
+            # Iterate over data.
+            for inputs, labels in dataloaders[phase]:
+                inputs = inputs.to(device)
+                labels = labels.to(device)
+
+                # zero the parameter gradients
+                optimizer.zero_grad()
+
+                # forward
+                # track history if only in train
+                with torch.set_grad_enabled(phase == 'train'):
+                    outputs = model(inputs)
+                    _, preds = torch.max(outputs, 1)
+                    loss = criterion(outputs, labels)
+
+                    # backward + optimize only if in training phase
+                    if phase == 'train':
+                        loss.backward()
+                        optimizer.step()
+
+                # statistics
+                running_loss += loss.item() * inputs.size(0)
+                running_corrects += torch.sum(preds == labels.data)
+
+            epoch_loss = running_loss / dataset_sizes[phase]
+            epoch_acc = running_corrects / dataset_sizes[phase]
+
+            #print('{} Loss: {:.4f} Acc: {:.4f}'.format(phase, epoch_loss, epoch_acc))
+
+            # save best validation accuracy
+            if phase == 'val' and epoch_acc > best_acc:
+                best_acc = epoch_acc
+
+    time_elapsed = time.time() - since
+    print('Training complete in {:.0f}m {:.0f}s'.format(time_elapsed // 60, time_elapsed % 60))
+    print('Best val Acc: {:4f}'.format(best_acc))
+
+    return best_acc
+
+
+def get_val_acc():
+  model_ft=TinyResnet()
+  model_ft = model_ft.to(device)
+  criterion = nn.CrossEntropyLoss()
+  optimizer_ft = optim.Adam(model_ft.parameters(), lr=lr)
+  acc = train_model(model_ft, criterion, optimizer_ft, dataloaders, dataset_sizes,num_epochs=3)
+  print(acc.cpu())
+  return acc.cpu()
